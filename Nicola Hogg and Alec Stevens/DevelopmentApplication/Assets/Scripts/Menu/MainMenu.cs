feecdcdbcdde667b5eb41927ac8aa858518138fd
DHaylock <david.haylock@watershed.co.uk>
2018-02-20 16:50:09 +0000

    - Added New Script Files for App

---
+++ Nicola Hogg and Alec Stevens/DevelopmentApplication/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+    public void StartApplication() {
+        print("Starting Application");
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OpenOptions() {
+        print("Opening Options");
+    }
+}
